2897918aa5b6b25ecf1bb44063d592a55ba6abdd
Orion <Orion@gmain.com>
2021-10-05 16:43:05 +0300

    Update new_tokens.js

--- server/DB_SEED/new_tokens.js
+++ server/DB_SEED/new_tokens.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-loop-func */
 const ethers = require('ethers');
+var async = require("async");
 
 const provider = new ethers.providers.WebSocketProvider('wss://bsc-ws-node.nariox.org:443');
 
@@ -23,81 +24,88 @@ const factoryContract = new ethers.Contract(factoryAddress, abi, provider);
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://192.168.112.98:27017";
-
-MongoClient.connect(url, function (err, db) {
+MongoClient.connect(url, async function (err, db) {
     if (err) throw err;
 
     var dbo = db.db("poocoin");
-    dbo.collection("tokens1").createIndex({ "token": 1 }, { unique: true });
+    dbo.collection("lpaddresses").createIndex({ "lp_address": 1 }, { unique: true });
+    dbo.collection("tokens").createIndex({ "token": 1 }, { unique: true });
+    var count_temp = await dbo.collection("lpaddresses").countDocuments();
+    async.forever(
+        function (StartSync) {
+            factoryContract.allPairsLength().then(async (count) => {
+                if (count > count_temp) {
+                    for (var i = count_temp; i < count; i++) {
+                        await factoryContract.allPairs(i).then(async (pairAddress) => {
+                            const pairContract = new ethers.Contract(pairAddress, abi, provider);
 
-    factoryContract.allPairsLength().then(count => {
-        setTimeout(async () => {
+                            let token0 = await pairContract.token0();
+                            const token0Contract = new ethers.Contract(token0, abi, provider);
+                            let token0Symbol = await token0Contract.symbol();
+                            let token0name = await token0Contract.name();
+                            let record = {};
 
-            for (var i = 410904; i < count; i++) {
-                await factoryContract.allPairs(i).then(pairAddress => {
-                    setTimeout(async () => {
-                        const pairContract = new ethers.Contract(pairAddress, abi, provider);
-                        let token0 = await pairContract.token0();
-                        // let token0 = await pairContract.token1();
+                            record.name = token0name;
+                            record.symbol = token0Symbol;
+                            record.token = token0;
 
-                        const token0Contract = new ethers.Contract(token0, abi, provider);
-                        console.log(pairContract);
-                        let token0Symbol = await token0Contract.symbol();
-                        let token0name = await token0Contract.name();
-                        let record = {};
+                            try {
+                                await dbo.collection("tokens").insertOne(record, function (err, res) {
+                                    if (err) console.log(err);
+                                    console.log("Number of documents inserted: " + i);
+                                    // db.close();
+                                });
+                            } catch (e) {
+                                console.log(e);
+                            }
+                            let token1 = await pairContract.token1();
 
-                        record.name = token0name;
-                        record.symbol = token0Symbol;
-                        record.token = token0;
+                            const token1Contract = new ethers.Contract(token1, abi, provider);
+                            let token1Symbol = await token1Contract.symbol();
+                            let token1name = await token1Contract.name();
 
-                        try {
-                            await dbo.collection("tokens1").insertOne(record, function (err, res) {
-                                if (err) console.log(err);
-                                console.log("Number of documents inserted: " + i);
-                                // db.close();
-                            });
-                        } catch (e) {
-                            console.log(e);
-                        }
-                        let token1 = await pairContract.token1();
+                            record.name = token1name;
+                            record.symbol = token1Symbol;
+                            record.token = token1;
 
-                        const token1Contract = new ethers.Contract(token1, abi, provider);
-                        let token1Symbol = await token1Contract.symbol();
-                        let token1name = await token1Contract.name();
+                            try {
+                                await dbo.collection("tokens").insertOne(record, function (err, res) {
+                                    if (err) console.log(err);
+                                    console.log("Number of documents inserted: " + i);
+                                    // db.close();
+                                });
+                            } catch (e) {
+                                console.log(e);
+                            }
 
-                        record.name = token1name;
-                        record.symbol = token1Symbol;
-                        record.token = token1;
+                            let lprecord = {}
 
-                        try {
-                            await dbo.collection("tokens1").insertOne(record, function (err, res) {
-                                if (err) console.log(err);
-                                console.log("Number of documents inserted: " + i);
-                                // db.close();
-                            });
-                        } catch (e) {
-                            console.log(e);
-                        }
+                            lprecord.token0 = token0;
+                            lprecord.token1 = token1;
+                            lprecord.type = "PancakeSwap";
+                            lprecord.lp_address = pairAddress;
 
-                        let lprecord = {}
-
-                        lprecord.token0 = token0;
-                        lprecord.token1 = token1;
-                        lprecord.type = "PancakeSwap";
-                        lprecord.lp_address = pairAddress;
-
-                        try {
-                            await dbo.collection("lpaddresses").insertOne(lprecord, function (err, res) {
-                                if (err) console.log(err);
-                                console.log("Number of documents inserted: " + i);
-                                // db.close();
-                            });
-                        } catch (e) {
-                            console.log(e);
-                        }
-                    }, 1);
-                });
-            }
-        }, 1);
-    });
+                            try {
+                                await dbo.collection("lpaddresses").insertOne(lprecord, function (err, res) {
+                                    if (err) console.log(err);
+                                    console.log("Number of documents inserted: " + i);
+                                    // db.close();
+                                });
+                            } catch (e) {
+                                console.log(e);
+                            }
+                        });
+                    }
+                    count_temp = count;
+                } else {
+                    console.log('current lp is up to date')
+                }
+                StartSync()
+            });
+        },
+        function (error) {
+            console.log(error);
+            StartSync()
+        }
+    );
 });
